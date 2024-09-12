73259a99fb06fd98b3bd2ef77fddd7df50f34c7b
Hamza Mukhtar <hamzamukhtar9819@gmail.com>
2024-09-12 06:03:27 +0000

    Finish simplejwt

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 
 from blog.api.views import PostList, PostDetail, UserDetail
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path("posts/", PostList.as_view(), name="api_post_list"),
     path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
     path("users/<str:email>", UserDetail.as_view(), name="api_user_detail"),
+    path("jwt/", TokenObtainPairView.as_view(), name="jwt_obtain_pair"),
+    path("jwt/refresh/", TokenRefreshView.as_view(), name="jwt_refresh"),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
