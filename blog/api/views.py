d2f8f30066e8e842172e78dd6bec1a3f105b7743
Hamza Mukhtar <hamzamukhtar9819@gmail.com>
2024-09-12 05:42:10 +0000

    Finish pagination and django filter

--- blog/api/views.py
+++ blog/api/views.py
@@ -1,11 +1,11 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
 
 from blog.api.serializers import PostSerializer, UserSerializer, PostDetailSerializer
 from blog.models import Post
 from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
 from blango_auth.models import User
 from blog.api.serializers import PostSerializer, UserSerializer
-
+from blog.api.filters import PostFilterSet
 
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
@@ -22,3 +22,10 @@ class UserDetail(generics.RetrieveAPIView):
     lookup_field = "email"
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    filterset_class = PostFilterSet
+    # filterset_fields can be removed
+    # other existing attributes and methods omitted
